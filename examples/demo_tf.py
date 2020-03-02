a1d7c1728577d8b38bf23a6d3f3978fab114485b
Dave Pagurek <davepagurek@gmail.com>
2020-03-02 10:33:28 -0800

    Scene setup

--- examples/demo_tf.py
+++ examples/demo_tf.py
@@ -17,16 +17,26 @@ import soft_renderer as sr
 current_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(current_dir, '../data')
 
+OPTIMIZE_SILHOUETTE = True
+
 
 class Model(nn.Module):
-    def __init__(self, template_path):
+    def __init__(self, template_path, bg_path):
         super(Model, self).__init__()
 
         # set template mesh
         self.template_mesh = sr.Mesh.from_obj(template_path)
+        self.bg_mesh = sr.Mesh.from_obj(bg_path)
         self.register_buffer('vertices', self.template_mesh.vertices * 0.5)
         self.register_buffer('faces', self.template_mesh.faces)
-        self.register_buffer('textures', self.template_mesh.textures)
+        self.register_buffer('bg_vertices', self.bg_mesh.vertices*3 + torch.tensor([0,-0.5,0]).repeat(1, self.bg_mesh.vertices.shape[1], 1).cuda())
+        self.register_buffer('bg_faces', self.bg_mesh.faces)
+        # Make the object black and background white
+        textures = torch.cat([
+            torch.tensor([0., 0., 0.]).repeat(1, self.template_mesh.vertices.shape[1], 1),
+            torch.tensor([1., 1., 1.]).repeat(1, self.bg_mesh.vertices.shape[1], 1),
+        ], dim=1)
+        self.register_buffer('all_textures', textures)
 
         # optimize for displacement map and center
         self.register_parameter('displace', nn.Parameter(torch.zeros_like(self.template_mesh.vertices)))
@@ -43,12 +53,17 @@ class Model(nn.Module):
         vertices = F.relu(vertices) * (1 - centroid) - F.relu(-vertices) * (centroid + 1)
         vertices = vertices + centroid
 
+        all_vertices = torch.cat([vertices, self.bg_vertices], dim=1)
+        idx_offset = self.template_mesh.vertices.shape[1]
+        all_faces = self.faces if OPTIMIZE_SILHOUETTE else torch.cat([self.faces, self.bg_faces + idx_offset], dim=1)
+
         # apply Laplacian and flatten geometry constraints
         laplacian_loss = self.laplacian_loss(vertices).mean()
         flatten_loss = self.flatten_loss(vertices).mean()
 
-        return sr.Mesh(vertices.repeat(batch_size, 1, 1), 
-                       self.faces.repeat(batch_size, 1, 1)), laplacian_loss, flatten_loss
+        return sr.Mesh(all_vertices.repeat(batch_size, 1, 1), 
+                       all_faces.repeat(batch_size, 1, 1),
+                       textures=self.all_textures.repeat(batch_size, 1, 1), texture_type='vertex'), laplacian_loss, flatten_loss
 
 
 def load_png(path, alpha_val=None):
@@ -58,7 +73,7 @@ def load_png(path, alpha_val=None):
         with_alpha = torch.cat((img, alpha), 2)
         img = with_alpha
 
-    return img.permute(2,0,1)
+    return img.permute(2, 0, 1)
 
 def neg_iou_loss(predict, target):
     dims = tuple(range(predict.ndimension())[1:])
@@ -69,23 +84,29 @@ def neg_iou_loss(predict, target):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--template-mesh', type=str, 
-        default=os.path.join(data_dir, 'obj/sphere/sphere_1352.obj'))
     parser.add_argument('-o', '--output-dir', type=str, 
         default=os.path.join(data_dir, 'results/output_deform'))
     args = parser.parse_args()
 
     os.makedirs(args.output_dir, exist_ok=True)
 
-    cameras = [
-        # eye, direction
-        ([-5, 0, 0], [1, 0, 0]),
-        ([0, 0, -5], [0, 0, 1]),
-    ]
+    cameras = []
+    # eye, direction
+    if OPTIMIZE_SILHOUETTE:
+        cameras.append(([-2, 0, 0], [1, 0, 0]))
+        cameras.append(([0, 0, -2], [0, 0, 1]))
+    else:
+        cameras.append(([2, 0, 0], [1, 0, 0]))
+        cameras.append(([0, 0, 2], [0, 0, 1]))
 
-    model = Model(args.template_mesh).cuda()
-    renderer = sr.SoftRenderer(image_size=64, sigma_val=1e-4, aggr_func_rgb='softmax', 
-                               camera_mode='look', viewing_angle=10)
+    # Use wider angle if placing the camera between the object and the wall
+    angle = 30 if OPTIMIZE_SILHOUETTE else 50
+
+    model = Model(os.path.join(data_dir, 'obj/sphere/colored_sphere.obj'), os.path.join(data_dir, 'obj/bg/bg_subdiv.obj')).cuda()
+    renderer = sr.SoftRenderer(image_size=64, sigma_val=1e-4, aggr_func_rgb='hard', 
+                               camera_mode='look', viewing_angle=angle,
+                               light_intensity_ambient=1., light_intensity_directionals=0.,
+                               background_color=[1.,1.,1.])
     renderer.transform.transformer._eye = [ eye for eye, _ in cameras ]
     renderer.transform.transformer.camera_direction = [ direction for _, direction in cameras ]
 
@@ -96,9 +117,13 @@ def main():
     ])
     optimizer = torch.optim.Adam(model.parameters(), 0.01, betas=(0.5, 0.99))
 
-    loop = tqdm.tqdm(list(range(0, 3000)))
+    loop = tqdm.tqdm(list(range(0, 2000)))
     writer = imageio.get_writer(os.path.join(args.output_dir, 'deform.gif'), mode='I')
     images_gt = images.cuda()
+    image1 = images_gt[:,0].detach().cpu().numpy()[0]#.transpose((1, 2, 0))
+    image2 = images_gt[:,0].detach().cpu().numpy()[1]#.transpose((1, 2, 0))
+    imageio.imsave(os.path.join(args.output_dir, 'gt1.png'), (255*image1).astype(np.uint8))
+    imageio.imsave(os.path.join(args.output_dir, 'gt2.png'), (255*image2).astype(np.uint8))
     for i in loop:
 
         mesh, laplacian_loss, flatten_loss = model(len(cameras))
@@ -106,9 +131,13 @@ def main():
 
         # optimize mesh with silhouette reprojection error and 
         # geometry constraints
-        loss = neg_iou_loss(images_pred[:, 3], images_gt[:, 0]) + \
-               0.0003 * flatten_loss
-               # 0.0003 * laplacian_loss + \
+        loss = 0.0003 * flatten_loss
+        if OPTIMIZE_SILHOUETTE:
+            loss += neg_iou_loss(
+                images_pred[:,-1],
+                1. - images_gt[:,0])
+        else:
+            loss += torch.nn.functional.l1_loss(images_pred[:,0], images_gt[:,0])
 
         loop.set_description('Loss: %.8f'%(loss.item()))
 
@@ -117,11 +146,11 @@ def main():
         optimizer.step()
 
         if i % 100 == 0:
-            image1 = images_pred.detach().cpu().numpy()[0].transpose((1, 2, 0))
-            image2 = images_pred.detach().cpu().numpy()[1].transpose((1, 2, 0))
-            writer.append_data((255*image1).astype(np.uint8))
-            imageio.imsave(os.path.join(args.output_dir, 'deform1_%05d.png'%i), (255*image1[..., -1]).astype(np.uint8))
-            imageio.imsave(os.path.join(args.output_dir, 'deform2_%05d.png'%i), (255*image2[..., -1]).astype(np.uint8))
+            image1 = images_pred[:,0].detach().cpu().numpy()[0]#.transpose((1, 2, 0))
+            image2 = images_pred[:,0].detach().cpu().numpy()[1]#.transpose((1, 2, 0))
+            writer.append_data((255*np.concatenate((image1, image2), axis=1)).astype(np.uint8))
+            imageio.imsave(os.path.join(args.output_dir, 'deform1_%05d.png'%i), (255*image1).astype(np.uint8))
+            imageio.imsave(os.path.join(args.output_dir, 'deform2_%05d.png'%i), (255*image2).astype(np.uint8))
 
     # save optimized mesh
     model(1)[0].save_obj(os.path.join(args.output_dir, 'tf.obj'), save_texture=False)
